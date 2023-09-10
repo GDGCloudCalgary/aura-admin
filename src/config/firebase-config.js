b2db6e6723ee5394851abe3af82a01224b38aa23
shaimaaashraf <shaimaaashraf@gmail.com>
2023-09-10 17:52:41 -0600

    add firebase admin

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -1,9 +1,10 @@
 export default {
-    apiKey: "AIzaSyCOl0qzkhCDmyWeeIT8AZe4xyJJlIfU5gE",
-    authDomain: "myaurapp.firebaseapp.com",
-    databaseURL: "https://myaurapp.firebaseio.com",
-    projectId: "myaurapp",
-    storageBucket: "myaurapp.appspot.com",
-    messagingSenderId: "419511997060",
-    appId: "1:419511997060:web:ce7aa0d308f6b484f868d1"
+    apiKey: "AIzaSyBcK0V1jk20A2U1kNVmwdbUMYFQ8X8uyjs",
+    authDomain: "gdgyycauraadmin.firebaseapp.com",
+    projectId: "gdgyycauraadmin",
+    storageBucket: "gdgyycauraadmin.appspot.com",
+    messagingSenderId: "336388771399",
+    appId: "1:336388771399:web:ce35ec46e4d94f3d36eb62",
+    measurementId: "G-FH4CFT02JT"
 }
+
